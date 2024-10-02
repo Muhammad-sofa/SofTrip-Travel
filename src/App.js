e8c13571aad402bcb1ebd14b0a5c79808190b8f4
Sofa <muhammadsofa52@yahoo.com>
2024-10-02 16:50:16 +0700

    Styling LandingPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import 'assets/scss/style.scss';
 
 function App() {
   return (
     <div className="App">
-      Hai !!
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
   );
 }
