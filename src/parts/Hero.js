afee285186590f8355b1912a040ce3a4c9bd1e0a
Sofa <muhammadsofa52@yahoo.com>
2024-10-04 10:55:51 +0700

    Update Component Button Hero

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -21,10 +21,10 @@ export default function Hero(props) {
           <section className='container' pt-4>
                <div className="row align-items-center">
                     <div className="col-auto pr-5" style={{ width: 530 }}>
-                         <h1 className="h2 font-weight-bold line-height-1 mb-3">
+                         <h1 className="font-weight-bold line-height-1 mb-3">
                               Forget busy work, <br /> Start Next Vacation
                          </h1>
-                         <p className="mb-5 font-weight-light text-gray-500 w-75">
+                         <p className="mb-5 font-weight-light text-gray-500 w-75" style={{ lineHeight: '170%' }}>
                               We provide what you need to enjoy your vacation. Time to make another memorable moments.
                          </p>
                          <Button className="btn px-5" hasShadow isPrimary onclick={showMostPicked}>
