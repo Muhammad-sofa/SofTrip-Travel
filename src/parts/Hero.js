ce7b3bffb7f47b8da8e3046aad15f84d73a6b82a
Sofa <muhammadsofa52@yahoo.com>
2024-10-04 10:11:44 +0700

    Styling Component Hero

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -20,7 +20,7 @@ export default function Hero(props) {
      return (
           <section className='container' pt-4>
                <div className="row align-items-center">
-                    <div className="col-auto pr-5" style={{ width: 422 }}>
+                    <div className="col-auto pr-5" style={{ width: 530 }}>
                          <h1 className="h2 font-weight-bold line-height-1 mb-3">
                               Forget busy work, <br /> Start Next Vacation
                          </h1>
@@ -32,13 +32,13 @@ export default function Hero(props) {
                          </Button>
 
                          <div className="row mt-5">
-                              <div className="col-auto">
+                              <div className="col-auto" style={{ marginRight: 35 }}>
                                    <img width="36" height="36" src={IconTraveler} alt={`${props.data.travelers} Travelers`}/>
                                    <h6 className='mt-3'>
                                         {props.data.travelers} <span className="text-gray-500 font-weight-light">Travelers</span>
                                    </h6>
                               </div>
-                              <div className="col-auto">
+                              <div className="col-auto" style={{ marginRight: 35 }}>
                                    <img src={IconTreasure} alt={`${props.data.treasures} Treasures`} />
                                    <h6 className='mt-3'>
                                         {props.data.treasures} <span className="text-gray-500 font-weight-light">Treasures</span>
@@ -50,6 +50,13 @@ export default function Hero(props) {
                                         {props.data.cities} <span className="text-gray-500 font-weight-light">Cities</span>
                                    </h6>
                               </div>
+                         </div>
+                    </div>
+
+                    <div className="col-6 pl-5">
+                         <div style={{ width: 520, height: 410 }}>
+                              <img src={ImageHero} alt="Room with couches" className="img-fluid position-absolute" style={{ margin: '-30px 0 0 -30px', zIndex: 1 }}/>
+                              <img src={ImageHero_} alt="Room with couches frame" className='img-fluid position-absolute' style={{ margin: '0 -15px -15px 0' }} />
                          </div>
                     </div>
                </div>
